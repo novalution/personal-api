9fedc5c636dc8cefa64875caaae9a860c5093c1e
novalution <57666913+novalution@users.noreply.github.com>
2021-03-15 09:20:53 +0700

    Update film.js

--- server/routers/film.js
+++ server/routers/film.js
@@ -6,23 +6,24 @@ async function getVideo(filems) {
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox']
 })
-  const page = await browser.newPage();
-  await page.goto('http://149.56.24.226/',{delay:1000});
-  await page.type('body > header > nav.top.navbar.navbar-inverse.hidden-xs > div > form > div > div > span.typeahead__query > input',`${filems}`,{delay:300})
-  await page.click('body > header > nav.top.navbar.navbar-inverse.hidden-xs > div > form > div > div > span.typeahead__button > button',{delay:1000})
-  await page.waitForSelector('body > main > div > section > div:nth-child(2) > div > div > div:nth-child(2) > div > div.col-xs-9.col-sm-10.search-content > h2 > a',{delay:1000})
-  await page.click('body > main > div > section > div:nth-child(2) > div > div > div:nth-child(2) > div > div.col-xs-9.col-sm-10.search-content > h2 > a',{delay:1000})
-  await page.waitForSelector('#download-movie > a.btn.btn-success',{delay:1000})
-  let video = await page.$eval('#download-movie > a.btn.btn-success', (element) => {
-      return element.getAttribute("href");
-      });
-  //   //let thumb = await page.$eval('#movie-detail > div > div.col-xs-2.content-poster > figure > img', (element) => {
-  //     return element.getAttribute("src");
-  //     });
-  let juduls = await page.$eval('#download-movie > h2', el => el.innerText);
-  let sinopsis = await page.$eval('#movie-detail > div > div.col-xs-10.content > blockquote', el => el.innerText);
+const page = await browser.newPage();
+await page.goto('http://149.56.24.226/',{delay:1000});
+await page.type('body > header > nav.top.navbar.navbar-inverse.hidden-xs > div > form > div > div > span.typeahead__query > input',`${filems}`,{delay:300})
+await page.type('body > header > nav.top.navbar.navbar-inverse.hidden-xs > div > form > div > div > span.typeahead__query > input',`${filems}`,{delay:300})
+await page.click('body > header > nav.top.navbar.navbar-inverse.hidden-xs > div > form > div > div > span.typeahead__button > button',{delay:1000})
+await page.waitForSelector('body > main > div > section > div:nth-child(2) > div > div > div:nth-child(2) > div > div.col-xs-9.col-sm-10.search-content > h2 > a',{delay:1000})
+await page.click('body > main > div > section > div:nth-child(2) > div > div > div:nth-child(2) > div > div.col-xs-9.col-sm-10.search-content > h2 > a',{delay:1000})
+await page.waitForSelector('#download-movie > a.btn.btn-success',{delay:1000})
+let video = await page.$eval('#download-movie > a.btn.btn-success', (element) => {
+    return element.getAttribute("href");
+    });
+//   //let thumb = await page.$eval('#movie-detail > div > div.col-xs-2.content-poster > figure > img', (element) => {
+//     return element.getAttribute("src");
+//     });
+let juduls = await page.$eval('#download-movie > h2', el => el.innerText);
+let sinopsis = await page.$eval('#movie-detail > div > div.col-xs-10.content > blockquote', el => el.innerText);
 
-  browser.close();
+browser.close();
   return { video, juduls,sinopsis}
 }
 film.get('/', async (req, res) => {
